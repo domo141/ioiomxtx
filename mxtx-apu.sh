454b31851ddcacac793bd96172937397ef3d0a34
Tomi Ollila <tomi.ollila@iki.fi>
2018-04-07 15:38:21 +0300

    hint how to update remote ref to pushed commit

--- mxtx-apu.sh
+++ mxtx-apu.sh
@@ -236,8 +236,8 @@ cmd_hints () # hints of some more acute ways to utilize mxtx tools
   " GIT_SSH_COMMAND='mxtx-rsh {link} . ssh' git clone git@ror:{repo}" \
   " GIT_SSH_COMMAND='mxtx-rsh {link} . ssh' git pull --rebase --autostash" '' \
   "Use mxtx-rsh as replacement for ssh:" \
-  " GIT_SSH_COMMAND='mxtx-rsh' git push {link}:{repopath} HEAD:new-master" '' \
-
+  " GIT_SSH_COMMAND='mxtx-rsh' git push {link}:{repopath} HEAD:new-master" \
+  "  (and on {link}: git update-ref refs/remotes/origin/master new-master)" ''
 }
 
 cmd_source () # check source of given '$0' command
