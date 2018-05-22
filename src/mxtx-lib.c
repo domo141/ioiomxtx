9ea65df64e5bc4e5551f46201587a83a1df2217c
Tomi Ollila <tomi.ollila@iki.fi>
2018-05-22 20:50:30 +0300

    src/mxtx-lib.c: xwritefully() to handle EAGAIN
    When output buffer is full in non-blocking (socket) fd, instead of
    bailing out, sleep 1 second and try again. Now also if write()
    returns 0 (which is interesting) sleep 1 second before retrying.
    
    Currently mxtx-rsh{,d} are the only programs using xwritefully().

--- src/mxtx-lib.c
+++ src/mxtx-lib.c
@@ -22,7 +22,7 @@
  *          All rights reserved
  *
  * Created: Wed 16 Aug 2017 21:06:56 EEST too
- * Last modified: Wed 14 Mar 2018 00:42:31 +0200 too
+ * Last modified: Tue 22 May 2018 20:49:20 +0300 too
  */
 
 /* sh mxtx-lib.c will compile single mxtx-lib.o -- good for testing
@@ -154,9 +154,10 @@ void xwritefully(int fd, const void * buf, ssize_t len)
         if (l >= len) return;
 
         if (l <= 0) {
-            if (l == 0) continue;
+            if (l == 0) { sleep(1); continue; }
+            if (errno == EAGAIN) { sleep(1); continue; }
             if (errno == EINTR) continue;
-            else die("write(%d, ...) failed:", fd);
+            die("write(%d, ..., %d) failed:", fd, len);
         }
         buf = (const char *)buf + l;
         len -= l;
